a5985cf8e207c4ed04f9794ba18aa6ed0df69e02
William Falcon <waf2107@columbia.edu>
2021-03-04 10:52:20 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,7 +3,9 @@ from argparse import Argumentparser
 
 parser = Argumentparser()
 parser = parser.add_arguments('--number', default=0, type=int)
+parser = parser.add_arguments('--food_item', default='burgers', type=str)
 args = parser.parse_args()
 
-print('hello world, ', args.number)
+print('hello world')
+print(f'I want to eat: {args.number} {args.food_item}')
 print(np.random.rand(10))
