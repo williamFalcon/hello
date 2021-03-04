b498bb6e25ccaa8be189d41f48ba63f6785a086f
William Falcon <waf2107@columbia.edu>
2021-03-04 10:54:48 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import numpy as np
 from argparse import Argumentparser
+import torch
 
 parser = Argumentparser()
 parser = parser.add_arguments('--number', default=0, type=int)
@@ -8,4 +9,5 @@ args = parser.parse_args()
 
 print('hello world')
 print(f'I want to eat: {args.number} {args.food_item}')
-print(np.random.rand(10))
+print('i can run any machine learning library like numpy, pytorch lightning, sklearn pytorch, keras, tensorflow')
+print(torch.rand(1), np.random.rand(1))
