f819ab690802ebc5a71d9768556fad57a6ee3dbf
William Falcon <waf2107@columbia.edu>
2021-03-04 10:49:02 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 import numpy as np
+from argparse import Argumentparser
 
+parser = Argumentparser()
+parser = parser.add_arguments('--number', default=0, type=int)
+args = parser.parse_args()
 
-print('hello world')
+print('hello world, ', args.number)
 print(np.random.rand(10))
