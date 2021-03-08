4e8fc9da4ce4e817dc5f392159deb46b17e660a1
William Falcon <waf2107@columbia.edu>
2021-03-07 19:28:27 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from argparse import ArgumentParser
 import torch
@@ -5,12 +6,19 @@ import torch
 parser = ArgumentParser()
 parser.add_argument('--number', default=0, type=int)
 parser.add_argument('--food_item', default='burgers', type=str)
+parser.add_argument('--data', default=None, type=str)
 args = parser.parse_args()
 
+if args.data is not None:
+	files = list(os.walk(args.data))
+	print('-' * 50)
+	print(f'DATA FOUND! {len(files)} files found at dataset {args.data}')
+
+
 print('-' * 50)
-print('hello world')
-print(f'There are {torch.cuda.device_count()} GPUs on this machine')
+print(f'GPUS: There are {torch.cuda.device_count()} GPUs on this machine')
 print('-' * 50)
-print(f'I want to eat: {args.number} {args.food_item}')
-print('i can run any machine learning library like numpy, pytorch lightning, sklearn pytorch, keras, tensorflow')
-print(torch.rand(1), np.random.rand(1))
+print(f'PARAMS: I want to eat: {args.number} {args.food_item}')
+print('-' * 50)
+print('i can run any ML library like numpy, pytorch lightning, sklearn pytorch, keras, tensorflow')
+print('torch:', torch.rand(1), 'numpy', np.random.rand(1))
