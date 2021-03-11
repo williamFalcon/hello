5506f858c02ba0387a6a15d9dd84d8f58d88afee
William Falcon <waf2107@columbia.edu>
2021-03-11 13:29:55 -0500

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
-
+# add arguments 
 parser = ArgumentParser()
 parser.add_argument('--number', default=0, type=int)
 parser.add_argument('--food_item', default='burgers', type=str)
@@ -14,15 +14,17 @@ args = parser.parse_args()
 # fake tensorboard logs
 writer = SummaryWriter()
 for x in range(1, 10000):
-    y = -np.log(x)
-    writer.add_scalar('y=-log(x)', y, x)
+	y = -np.log(x)
+	writer.add_scalar('y=-log(x)', y, x)
 writer.close()
 
+# print data if available
 if args.data is not None:
-    files = list(os.walk(args.data))
-    print('-' * 50)
-    print(f'DATA FOUND! {len(files)} files found at dataset {args.data}')
+	files = list(os.walk(args.data))
+	print('-' * 50)
+	print(f'DATA FOUND! {len(files)} files found at dataset {args.data}')
 
+# print GPUs, params and random tensors
 print('-' * 50)
 print(f'GPUS: There are {torch.cuda.device_count()} GPUs on this machine')
 print('-' * 50)
@@ -30,3 +32,8 @@ print(f'PARAMS: I want to eat: {args.number} {args.food_item}')
 print('-' * 50)
 print('i can run any ML library like numpy, pytorch lightning, sklearn pytorch, keras, tensorflow')
 print('torch:', torch.rand(1), 'numpy', np.random.rand(1))
+
+# write some artifacts
+f = open("weights.ckpt", "a")
+f.write("fake weights")
+f.close()
